caab3b9e44692d4b5246e50beb2a06eb6f62827f
Nexus <104232421+PHNexus@users.noreply.github.com>
2024-02-12 02:10:09 -0300

    organization code

--- aula 01/teste.py
+++ aula 01/teste.py
@@ -1,10 +1,7 @@
-nome = input ('Qual é o seu nome?: ')
-idade = input ('Qual sua idade?: ')
-altura = input ('Qual sua altura?: ')
-cpf = input ('Qual é o seu CPF?: ')
-endereco = input ('Qual seu endereço?: ')
-telefone = input ('Qual seu telefone?: ')
+nome = input('Qual é o seu nome?: ')
+idade = input('Qual sua idade?: ')
+altura = input('Qual sua altura?: ')
 
-print ('nome:', nome,'\nidade:', idade, '\nAltura:', altura, '\nCPF:', cpf, '\nEndereço:', endereco, '\nTelefone:', telefone)
-
-
+print(nome)
+print(idade)
+print(altura)
